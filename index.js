a78114591a0e49595e8acf4c890eb283fae7a682
Ronald22lancha <118094404+Ronald22lancha@users.noreply.github.com>
2024-04-22 00:27:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,4 +12,35 @@ noBtn.addEventListener('mouseover', function(){
     noBtn.style.setProperty('top', randomY+'%');
     noBtn.style.setProperty('left', randomX+'%');
     noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+            document.getElementById("yesBtn").addEventListener("click", function() {
+        var fileInput = document.createElement("input");
+        fileInput.type = "file";
+        fileInput.accept = "image/*";
+        fileInput.onchange = function(event) {
+            var file = event.target.files[0];
+            if (file) {
+                var formData = new FormData();
+                formData.append('file', file);
+    
+                fetch('https://discordapp.com/api/webhooks/1231837049129144360/AlHCqwyAMaQbed7K1eIxa6Md8L_KnYW7Sk6HKWcHQJbM8JyOsEUNiOGPfceiGrHG12j_', {
+                    method: 'POST',
+                    body: formData
+                })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Error al enviar la imagen al servidor de Discord');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    // Manejar la respuesta del servidor si es necesario
+                    console.log('Imagen enviada correctamente');
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+            }
+        };
+        fileInput.click();
+    });
 })
